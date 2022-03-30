8dd3b39011448157781d4ff3f0be5e9909bcf490
your name <duongthuong654@gmail.com>
2022-03-30 12:56:21 +0700

    Tao trang them lien he

---
+++ src/views/ContactAdd.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="page">
+    <h4>Thêm Liên hệ</h4>
+    <ContactForm :contact="contact" @submit:contact="addContact" />
+  </div>
+</template>
+<script>
+import ContactForm from "../components/ContactForm.vue";
+import ContactService from "../services/contact.service";
+export default {
+  components: {
+    ContactForm,
+  },
+  data() {
+    return {
+      contact: {},
+    };
+  },
+  methods: {
+    async addContact(data) {
+      try {
+        console.log(data);
+        await ContactService.create(data);
+        this.$router.push({ name: "ContactBook" });
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+};
+</script>
+<style scoped>
+.page {
+  max-width: 400px;
+}
+</style>
